5d21a041a07f4bb85d0420afb48996b95a5a0113
Harsh Vardhan Soni <sonihv01@gmail.com>
2026-04-30 10:58:41 +0530

    added dashboard

---
+++ src/app/api/dashboard/page.js
@@ -0,0 +1,11 @@
+"use client";
+
+import { Dashboard } from "../../../../components/dashboard/dashboard";
+
+export default function DashboardPage() {
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,18 @@
 import { Dashboard } from "../../components/dashboard/dashboard";
-
+import { useSession } from "next-auth/react";
 export default function Home() {
+  const session = useSession();
   return (
-    <div>
-      <Dashboard />
-    </div>
+    <>
+      {session.status === "authenticated" ? (
+        <Dashboard />
+      ) : (
+        <div className="flex justify-center items-center h-screen">
+          <h1 className="text-2xl font-bold">
+            Please login to access the dashboard
+          </h1>
+        </div>
+      )}
+    </>
   );
 }
